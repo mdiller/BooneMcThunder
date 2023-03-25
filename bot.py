0340f31333aab01f6f0e5c3ed2137c718d1b5649
Malcolm Diller <malcolm@dillerm.io>
2023-03-24 18:32:08 -0700

    fixed a couple things and added a token limiter, and made it more modular and easier to add more pr prompts

--- bot.py
+++ bot.py
@@ -2,16 +2,15 @@ import conversator as conv
 import disnake
 from disnake.ext import commands
 import json
+import re
 
-with open("config_dev.json", "r") as f:
+with open("config.json", "r") as f:
 	config = json.loads(f.read())
 
 config_bot_token = config["bot_token"]
 config_api_key = config["api_key"]
 config_guild_ids = config["guild_ids"]
 config_channel_ids = config["channel_ids"]
-config_first_message_role = config.get("first_message_role")
-
 
 intents = disnake.Intents.default()
 intents.message_content = True
@@ -22,18 +21,22 @@ bot = commands.Bot(
 
 conversator = conv.Conversator(config_api_key, bot.loop)
 
-conversator.input_system("Talk in a hillbilly accent and respond to all messages in a single sentance")
-conversator.input_system("Be very witty and clever in your responses")
-conversator.input_system("Be very rude to everyone")
+first_message = None
+first_message_regex = "START: (.*)\n"
 
-first_message_role = None
+prompt_file = config.get("prompt")
+if prompt_file:
+	with open(prompt_file, "r") as f:
+		text = f.read()
+	match_start = re.search(first_message_regex, text)
+	if match_start:
+		first_message = match_start.group(1)
+		text = re.sub(first_message_regex, "", text)
+	conversator.input_system(text)
+	print(f"loaded: {prompt_file}")
+	print(text)
 
-if config_first_message_role:
-	first_message_role = config_first_message_role
-
-first_message = "Ya'll are dumber than a box of rocks"
-
-first_run=True
+first_run = True
 
 @bot.event
 async def on_ready():
@@ -44,15 +47,13 @@ async def on_ready():
 		name = bot.user.name
 		conversator.input_system(f"Your name is '{name}'")
 		channel = bot.get_channel(config_channel_ids[0])
-		conversator.input_self(first_message)
-		if first_message_role:
-			first_message = f"<@&{first_message_role}> {first_message}" 
-		await channel.send(first_message)
+		if first_message:
+			conversator.input_self(first_message)
+			await channel.send(first_message)
 		first_run = False
 
 @bot.event
 async def on_message(message: disnake.Message):
-	print(message.content)
 	if message.channel.id not in config_channel_ids or message.author.id == bot.user.id:
 		return
 	if message.content.startswith("// ") or message.content.startswith("("):
@@ -60,6 +61,7 @@ async def on_message(message: disnake.Message):
 	conversator.input_user(message.clean_content)
 	async with message.channel.typing():
 		response = await conversator.get_response()
+		response = response.lower()
 		await message.channel.send(response)
 
 if __name__ == '__main__':
--- conversator.py
+++ conversator.py
@@ -3,20 +3,40 @@ import openai
 import functools
 from concurrent.futures import ThreadPoolExecutor
 
+MAX_TOKENS_STORED = 3300
+
 class Conversator:
 	def __init__(self, api_key, loop):
 		openai.api_key = api_key
 		self.messages = []
+		self.token_counts = []
+		self.tokens_total = 0
 		self.loop = loop
 
+	def _input_message(self, role, message):
+		count = len(message.split(" "))
+		self.messages.append({"role": role, "content": message})
+		self.token_counts.append(count)
+		self.tokens_total += count
+
+		# make sure we don't go over the limit of tokens stored. delete non-system messages from beginning to allow for this.
+		i = 0
+		while self.tokens_total > MAX_TOKENS_STORED:
+			if self.messages[i]["role"] == "system":
+				i += 1
+			else:
+				self.tokens_total -= self.token_counts[i]
+				del self.token_counts[i]
+				del self.messages[i]
+
 	def input_system(self, message):
-		self.messages.append({"role": "system", "content": message})
+		self._input_message("system", message)
 
 	def input_user(self, message):
-		self.messages.append({"role": "user", "content": message})
+		self._input_message("user", message)
 
 	def input_self(self, message):
-		self.messages.append({"role": "assistant", "content": message})
+		self._input_message("assistant", message)
 	
 	def _get_response(self):
 		response = openai.ChatCompletion.create(
